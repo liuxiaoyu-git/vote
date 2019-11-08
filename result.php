a527a02c86a8f1c137efa81bf75ed3910b48d180
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-08 19:32:56 +0800

    Create result.php

---
+++ result.php
@@ -0,0 +1,22 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER"); #openshift
+$dbname = getenv("DATABASE_NAME"); #sampledb
+$dbpwd = getenv("DATABASE_PASSWORD"); #password
+$username = trim($_POST['username']);
+$code = trim($_POST['code']);
+$result = trim($_POST['prizetype']);
+$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
+$sql = "insert into PrizeWheel values('".$username."-".$code."','".$prizetype."')";
+
+if ($connection->query($sql) !== TRUE) {  
+	echo "发生数据库操作错误或用户".$username."已经存在";
+} else {
+	echo "您使用了以下信息参加本轮游戏：";
+	echo "<br/>用户名：".$username;
+	echo "<br/>手机后4位：".$code;
+	echo "<br/>抽奖结果：".$prizetype;
+}
+mysqli_close($connection);
+?>
