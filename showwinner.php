852a729ffadcfcc44573dacdb1a880548c26a7cd
dawnsky@sina.com <dawnsky@sina.com>
2019-11-13 12:53:25 +0000

    add a space3

--- showwinner.php
+++ showwinner.php
@@ -7,6 +7,7 @@ $dbpwd = getenv("DATABASE_PASSWORD"); #password
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 $sql = "select * from PrizeWheel order by user";
 $rs = $connection->query($sql);
+
 echo "<table><tr><td>抽奖人</td><td>获奖情况</td></tr>";
 while($row = mysqli_fetch_assoc($rs))
 	echo "<tr><td>".$row['user']."</td><td>".$row['prizetype']."</td></tr>";
