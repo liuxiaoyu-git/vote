28c9614cdf0bb4f3c72a8e5d111212aa83e1566e
redhatdemo <redhatdemo@demo.com>
2021-07-18 09:40:55 +0000

    更新 'index.php'

--- index.php
+++ index.php
@@ -1,87 +1,42 @@
-<ht<html>
-<head>
-    <title>转盘抽奖</title>
-    <!-- 这里是css部分 -->
-    <style>
-        #bg {
-            width: 650px;
-            height: 600px;
-            margin: 0 auto;
-            background: url(turntable-bg.jpg) no-repeat;
-            position: relative;
-        }
-        img[src^="pointer"] {
-            position: absolute;
-            z-index: 10;
-            top: 155px;
-            left: 247px;
-        }
-        img[src^="turntable"] {
-            position: absolute;
-            z-index: 5;
-            top: 60px;
-            left: 116px;
-            transition: all 4s;
-        }
-    </style>
+<DOCTYPE HTML PUBLIC "-//W3C//DTDHTML 4.0 Transitional//EN">
+<html>
+	<HEAD>
+		<title>投票</title>
+<style type="text/css">
+#main{width:100%;min-height:100%;height:100%;overflow:hidden !important;overflow: visible;}
+.style1 {
+	font-size: large;
+	color: #FF0000;
+}
+</style>
 </head>
 <body>
-<form id="prizewheel" method="post" action="result.php">
-    <table width="100%" height="100%">
+<table id="main">
+	<tr>
+	<td align="center">
+	<form name="vote" method="post" action="result.php">
+	<table>
 		<tr>
-            <td align="center">请先填写完抽奖信息后再抽奖！抽奖即可。</td>
-	    </tr>		<tr>
-            <td align="center">姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名：<input name="username" type="text"><input type="hidden" id="prizetype" name="prizetype" value="-----" /></td>
-	    </tr>
+			<td colspan="2" align="left" height="100" valign="top"><img src="banner.png" width="350"></td>
+		</tr>
 		<tr>
-            <td align="center">手机4位：<input name="code" type="text"/></td>
-	    </tr>
+			<td colspan="2" height="50" valign="top" class="style1">选出一项你希望新版本更新的内容：</td>
+		</tr>
 		<tr>
-			<td align="center">
-	<!-- 这里是HTML结构部分 -->
-    <div id="bg"><img src="pointer.png" alt="pointer"><img src="turntable.png" alt="turntable"></div>
-    <!-- 这里是js部分 -->
-    <script>
-        var oPointer = document.getElementsByTagName("img")[0];
-        var oTurntable = document.getElementsByTagName("img")[1];
-        var cat = 51.4; //总共7个扇形区域，每个区域约51.4度
-        var num = 0; //转圈结束后停留的度数
-        var offOn = true; //是否正在抽奖
-        oPointer.onclick = function () {
-            if (offOn) {
-                oTurntable.style.transform = "rotate(0deg)";
-                offOn = !offOn;
-                ratating();
-            }
-        }
-        //旋转
-        function ratating() {
-            var timer = null;
-            var rdm = 0; //随机度数
-            clearInterval(timer);
-            timer = setInterval(function () {
-                if (Math.floor(rdm / 360) < 3) { rdm = Math.floor(Math.random() * 3600); }
-                else {
-                    oTurntable.style.transform = "rotate(" + rdm + "deg)";
-                    clearInterval(timer);
-                    setTimeout(function () {
-                        offOn = !offOn;
-                        num = rdm % 360;
-                        if (num <= cat * 1) { document.getElementById("prizetype").value="面单999元"; document.getElementById("prizewheel").submit();}
-                        else if (num <= cat * 2) { document.getElementById("prizetype").value="免单50元"; document.getElementById("prizewheel").submit();}
-                        else if (num <= cat * 3) { document.getElementById("prizetype").value="免单10元"; document.getElementById("prizewheel").submit();}
-                        else if (num <= cat * 4) { document.getElementById("prizetype").value="免单5元"; document.getElementById("prizewheel").submit();}
-                        else if (num <= cat * 5) { document.getElementById("prizetype").value="免分期"; document.getElementById("prizewheel").submit();}
-                        else if (num <= cat * 6) { document.getElementById("prizetype").value="白条额度"; document.getElementById("prizewheel").submit();}
-                        else if (num <= cat * 7) { document.getElementById("prizetype").value="未中奖"; document.getElementById("prizewheel").submit();}
-                    }, 4000);
-                }
-            }, 30);
-        }
-    </script>
+			<td>
+				<select name="vote">
+				  <option value="all">更新和游戏背景和游戏宝藏</option>
+				  <option value="background">只更新游戏背景</option>
+				  <option value="target">只更新游戏宝藏</option>
+				</select>
 			</td>
-	    </tr>
-    </table>
-</form>
+			<td><input type="submit" value="投票"></td>
+		</tr>	
+	</table>
+	</form>
+	</td>
+	</tr>
+</table>
+
 </body>
 </html>
