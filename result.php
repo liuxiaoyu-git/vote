074029247d96bf2954a6e8bbc3f4499772dc5e33
redhatdemo <redhatdemo@demo.com>
2021-07-18 13:05:32 +0000

    更新 'result.php'

--- result.php
+++ result.php
@@ -4,16 +4,29 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("MYSQL_USER"); #openshift
 $dbname = getenv("MYSQL_DATABASE"); #sampledb
 $dbpwd = getenv("MYSQL_PASSWORD"); #password
-$username = trim($_POST['username']);
-$code = trim($_POST['code']);
-$prizetype = trim($_POST['prizetype']);
-$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
-$sql = "insert into PrizeWheel values('".$username."-".$code."','".$prizetype."')";
 
-$connection->query($sql);
-	echo "<br/>用户名：".$username;
-	echo "<br/>手机后4位：".$code;
-	echo "<br/>抽奖结果：".$prizetype;
+$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
+$sql = "select vote_item, count(vote_item) vote_count from vote group by vote_item";
+
+
+        $res=mysql_query($sql,$connection);
+        $rows=mysql_affected_rows($connection);//获取行数
+        $colums=mysql_num_fields($res);//获取列数
+        
+        echo "<table><tr>";
+        for($i=0; $i < $colums; $i++){
+            $field_name=mysql_field_name($res,$i);
+            echo "<th>$field_name</th>";
+        }
+        echo "</tr>";
+        while($row=mysql_fetch_row($res)){
+            echo "<tr>";
+            for($i=0; $i<$colums; $i++){
+                echo "<td>$row[$i]</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
 
 mysqli_close($connection);
 ?>
