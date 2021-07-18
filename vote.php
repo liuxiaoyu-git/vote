7a0b2e2731ed11e9708bed956181abe605810fb8
redhatdemo <redhatdemo@demo.com>
2021-07-18 09:59:23 +0000

    更新 'vote.php'

--- vote.php
+++ vote.php
@@ -5,20 +5,21 @@ $dbuser = getenv("DATABASE_USER"); #openshift
 $dbname = getenv("DATABASE_NAME"); #demodb
 $dbpwd = getenv("DATABASE_PASSWORD"); #password
 
-echo "dbhost".$dbhost;
-echo "dbport".$dbport;
-echo "dbuser".$dbuser;
-echo "dbname".$dbname;
-echo "dbpwd".$dbpwd;
+echo "<br>dbhost:".$dbhost;
+echo "<br>dbport:".$dbport;
+echo "<br>dbuser".$dbuser;
+echo "<br>dbname:".$dbname;
+echo "<br>dbpwd:".$dbpwd;
 
 
 $vote = trim($_POST['vote']);
 $sql = "insert into vote values('".$vote."')";
+echo "<br>sql:".$sql;
 
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 $connection->query($sql);
 
-echo "投票结果：".$vote;
+echo "<br>投票结果：".$vote;
 
 mysqli_close($connection);
 ?>
