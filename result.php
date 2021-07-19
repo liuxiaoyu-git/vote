2115809b1d2230c03020af437c280d5c7784bcc7
liuxiaoyu-git <xiaoyliu@redhat.com>
2021-07-19 20:16:55 +0800

    Update result.php

--- result.php
+++ result.php
@@ -9,18 +9,29 @@ $sql = "select vote_item, count(vote_item) vote_count from vote group by vote_it
 $rs = $connection->query($sql);
 ?>
 <html>
-<div id="box">
-  <table width="100%" height="100%">
-    <tr>
-      <td align="center"><font size="26" color="#FF0000">投票更新内容</font></td><td align="center"><font size="26" color="#FF0000">投票总数量</font></td>
-    </tr>
-    <?php
-    while ($row = mysqli_fetch_assoc($rs)) {
-      echo "<tr><td align=center><font size=26>".$row['vote_item']."</font></td>";
-      echo "<td align=center><font size=26>".$row['vote_count']."</font></td></tr>";
-    }
-    mysqli_close($connection);
-    ?>
-  </table>
-</div>
+  <head>
+    <meta http-equiv="refresh" content="5">
+  </head>
+<table width="100%" height="100%">
+	<tr>
+	<td align="center">
+	<table>
+      <tr width=600 height=150>
+        <td colspan="2" align="center" height="100" valign="top"><img src="banner.png" width="500"></td>
+      </tr>
+      <tr>
+        <td align="center"><font size="24px" color="#FF0000">投票更新内容</font></td>
+        <td align="center"><font size="24px" color="#FF0000">投票总数量</font></td>
+      </tr>
+      <?php
+      while ($row = mysqli_fetch_assoc($rs)) {
+        echo "<tr><td align=center><font size=24px>".$row['vote_item']."</font></td>";
+        echo "<td align=center><font size=24px>".$row['vote_count']."</font></td></tr>";
+      }
+      mysqli_close($connection);
+      ?>
+	</table>
+	</td>
+	</tr>
+</table>
 </html>
